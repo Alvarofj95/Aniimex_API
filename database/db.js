417b8deee14d1aea27c720e610a3b602bd3fd4f1
Alvaro <alvarofj95@gmail.com>
2022-02-21 23:12:20 +0100

    Api finished 2.0

--- database/db.js
+++ database/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/db'
-    //db: 'mongodb+srv://Aniimex:passAniimex@cluster0.5yqpe.mongodb.net/aniimex?authSource=admin'
+    //db: 'mongodb://localhost:27017/db'
+    db: 'mongodb+srv://Aniimex:passAniimex@cluster0.5yqpe.mongodb.net/aniimex?authSource=admin'
 };
